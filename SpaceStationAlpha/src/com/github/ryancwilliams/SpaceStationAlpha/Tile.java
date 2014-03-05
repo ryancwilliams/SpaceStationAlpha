d224939d01c4e1d09a5444c0af60338ffea15a68
Ryan Williams <rcw4rcw@gmail.com>
2014-03-05 18:14:52 -0500

    Implemented Renderable in Tile

--- SpaceStationAlpha/src/com/github/ryancwilliams/SpaceStationAlpha/Tile.java
+++ SpaceStationAlpha/src/com/github/ryancwilliams/SpaceStationAlpha/Tile.java
@@ -13,23 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.github.ryancwilliams.SpaceStationAlpha;
 
 import com.github.ryancwilliams.SpaceStationAlpha.graphics.Renderable;
+import com.github.ryancwilliams.SpaceStationAlpha.graphics.Sprite;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 
 /**
+ * A basic Tile
  *
  * @author ryancwilliams
  */
 public class Tile implements Renderable {
-    
+
     /**
-     * The image of this Tile.
+     * The sprite that is displayed when this is rendered.
      */
-    private Image image;
+    protected Sprite sprite;
     /**
      * The X coordinate on the tile grid.
      */
@@ -38,26 +39,50 @@ public class Tile implements Renderable {
      * The Y coordinate on the tile grid.
      */
     private int coordY;
+    /**
+     * The width of this tile on the tile grid.
+     */
+    private final int WIDTH;
+    /**
+     * The height of this tile on the tile grid.
+     */
+    private final int HEIGHT;
 
     /**
      * Creates a new Tile.
-     * @param image The image to use for this tile
-     * @param coordX The X coordinate of this tile on the tile grid. 
-     * @param coordY The Y coordinate of this tile on the tile grid.
+     *
+     * @param sprite The sprite to use for this tile
+     * @param coordX The X coordinate of this tile on the tile grid
+     * @param coordY The Y coordinate of this tile on the tile grid
+     * @param width The width of this tile on the tile grid
+     * @param height he height of this tile on the tile grid
      */
-    public Tile(Image image, int coordX, int coordY) {
-        this.image = image;
+    public Tile(Sprite sprite, int coordX, int coordY, int width, int height) {
+        this.sprite = sprite;
         this.coordX = coordX;
         this.coordY = coordY;
+        this.WIDTH = width;
+        this.HEIGHT = height;
+    }
+
+    /**
+     * Creates a new 1 by 1 Tile.
+     *
+     * @param sprite The sprite to use for this tile
+     * @param coordX The X coordinate of this tile on the tile grid
+     * @param coordY The Y coordinate of this tile on the tile grid
+     */
+    public Tile(Sprite sprite, int coordX, int coordY) {
+        this(sprite, coordX, coordY, 1, 1);
     }
 
     @Override
     public void initialize() {
-        
+
     }
 
     @Override
     public void render(Graphics g, int x, int y) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        sprite.render(g, x, y);
     }
 }
