05b29998ec62c69bfcdb59913b43ee8472bae2d4
Ryan Williams <rcw4rcw@gmail.com>
2014-03-05 16:38:29 -0500

    Added Constructors to Sprite Sheet

--- SpaceStationAlpha/src/com/github/ryancwilliams/SpaceStationAlpha/graphics/SpriteSheet.java
+++ SpaceStationAlpha/src/com/github/ryancwilliams/SpaceStationAlpha/graphics/SpriteSheet.java
@@ -13,24 +13,56 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.github.ryancwilliams.SpaceStationAlpha.graphics;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 
 /**
  * A sheet of sprites that can be converted to sprites to be drawn individually.
+ *
  * @author ryancwilliams
  */
 public class SpriteSheet {
-    
+
     /**
      * The image this sheet is based off of.
      */
     private Image source;
+
+    /**
+     * Create a new sprite sheet using the provided image as the source.
+     *
+     * @param source The image to base the sprite sheet off of.
+     */
+    public SpriteSheet(Image source) {
+        this.source = source;
+    }
+
+    /**
+     * Creates a new sprite sheet using the specified location as the source.
+     * @param ref The location of the image file to load
+     * @throws SlickException Indicates a failure to load the image
+     */
+    public SpriteSheet(String ref) throws SlickException {
+        this(new Image(ref));
+    }
+
+    /**
+     * Creates a new sprite sheet using the specified location as the source.
+     * This constructor allows for a color to be used to mark transparency.
+     * @param ref The location of the image file to load
+     * @param trans The color to be treated as transparent
+     * @throws SlickException Indicates a failure to load the image
+     */
+    public SpriteSheet(String ref, Color trans) throws SlickException {
+        this(new Image(ref, trans));
+    }
     
     /**
      * Extracts a sub-image from this sprite sheet.
+     *
      * @param x The x coordinate of the sub-image
      * @param y The y coordinate of the sub-image
      * @param width The width of the sub-image
