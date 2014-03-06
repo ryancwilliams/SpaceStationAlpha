6dec8ed933b222eb218e5392c35f3540c6dbd300
Ryan Williams <rcw4rcw@gmail.com>
2014-03-05 19:11:15 -0500

    Added Name Field to Sprite.

--- SpaceStationAlpha/src/com/github/ryancwilliams/SpaceStationAlpha/graphics/Sprite.java
+++ SpaceStationAlpha/src/com/github/ryancwilliams/SpaceStationAlpha/graphics/Sprite.java
@@ -19,7 +19,9 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 
 /**
- * A sprite that can be drawn.
+ * A sprite that can be drawn. Sprites have names attached to them to make
+ * working with a registry easer. Sprites without names are given the name of
+ * null and can not be loaded into a registry.
  *
  * @author ryancwilliams
  */
@@ -28,19 +30,51 @@ public class Sprite implements Renderable {
     /**
      * The image of this sprite
      */
-    private final Image sprite;
+    private final Image SPRITE;
+    /**
+     * The name of this sprite
+     */
+    private final String NAME;
 
     /**
-     * Creates a sprite from a image.
+     * Creates a named sprite from a image.
+     *
+     * @param name The name of this sprite.
+     * @param image The image to create the sprite from
+     */
+    public Sprite(String name, Image image) {
+        this.NAME = name;
+        this.SPRITE = image;
+    }
+
+    /**
+     * Creates a sprite from a image. This sprite will be nameless so it can not
+     * be loaded into a registry.
      *
      * @param image The image to create the sprite from
      */
     public Sprite(Image image) {
-        this.sprite = image;
+        this(null, image);
     }
 
     /**
-     * Creates a sprite using a image extracted from a sprite sheet.
+     * Creates a named sprite using a image extracted from a sprite sheet.
+     *
+     * @param name The name of this sprite.
+     * @param spriteSheet The Sprite Sheet to extract from
+     * @param x The x coordinate of the image on the sprite sheet.
+     * @param y The y coordinate of the image on the sprite sheet.
+     * @param width The width of the image on the sprite sheet.
+     * @param height The height of the image on the sprite sheet.
+     */
+    public Sprite(String name, SpriteSheet spriteSheet,
+            int x, int y, int width, int height) {
+        this(name, spriteSheet.extract(x, y, width, height));
+    }
+
+    /**
+     * Creates a sprite using a image extracted from a sprite sheet. This sprite
+     * will be nameless so it can not be loaded into a registry.
      *
      * @param spriteSheet The Sprite Sheet to extract from
      * @param x The x coordinate of the image on the sprite sheet.
@@ -49,7 +83,7 @@ public class Sprite implements Renderable {
      * @param height The height of the image on the sprite sheet.
      */
     public Sprite(SpriteSheet spriteSheet, int x, int y, int width, int height) {
-        this(spriteSheet.extract(x, y, width, height));
+        this(null, spriteSheet, x, y, width, height);
     }
 
     /**
@@ -58,7 +92,7 @@ public class Sprite implements Renderable {
      * @return the image of this sprite.
      */
     public Image getSprite() {
-        return sprite;
+        return SPRITE;
     }
 
     /**
@@ -67,7 +101,7 @@ public class Sprite implements Renderable {
      * @return the width of this sprite
      */
     public int getWidth() {
-        return this.sprite.getWidth();
+        return this.SPRITE.getWidth();
     }
 
     /**
@@ -76,16 +110,16 @@ public class Sprite implements Renderable {
      * @return The height of this sprite.
      */
     public int getHeight() {
-        return this.sprite.getHeight();
+        return this.SPRITE.getHeight();
     }
 
     @Override
     public void initialize() {
-        
+
     }
 
     @Override
     public void render(Graphics g, int x, int y) {
-        g.drawImage(this.sprite, x, y);
+        g.drawImage(this.SPRITE, x, y);
     }
 }
