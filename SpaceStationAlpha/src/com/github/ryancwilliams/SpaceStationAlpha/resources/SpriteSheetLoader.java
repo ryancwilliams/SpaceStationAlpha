e5a2bd8ffdbb4b976cb7995963101b4e57c684f7
Ryan Williams <rcw4rcw@gmail.com>
2014-03-07 12:50:38 -0500

    Started work on SpriteSheetLoader class.

---
+++ SpaceStationAlpha/src/com/github/ryancwilliams/SpaceStationAlpha/resources/SpriteSheetLoader.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2014 ryancwilliams.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.ryancwilliams.SpaceStationAlpha.resources;
+
+import com.github.ryancwilliams.SpaceStationAlpha.graphics.Sprite;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  
+ * @author ryancwilliams
+ */
+public class SpriteSheetLoader {
+    
+    public static List<Sprite> readSprites(FileInputStream xmlFile) {
+        List<Sprite> sprites = new ArrayList<>();
+        
+        //TODO: Work on sprite loader.
+        
+        return sprites;
+    }
+    
+}
